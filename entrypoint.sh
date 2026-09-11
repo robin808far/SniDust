4e04b774d1f6c73b75340ff32db22ff7333c5367
robin808far <123860561+robin808far@users.noreply.github.com>
2026-09-11 20:56:46 +0330

    Refactor entrypoint.sh and add client file handling
    Refactor entrypoint.sh to improve readability and add client file creation.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-# Validate DoT config
 if [ "${DNSDIST_ENABLE_DOT}" == "true" ]; then
   VALID_CERT_TYPE_VALUES=("auto-self" "manual")
   if [[ -z "$DNSDIST_DOT_CERT_TYPE" ]]; then
@@ -18,46 +17,44 @@ if [ "${DNSDIST_ENABLE_DOT}" == "true" ]; then
   fi
 fi
 
-if [ -z "${EXTERNAL_IP}" ];
-then
+if [ -z "${EXTERNAL_IP}" ]; then
   echo "[INFO] External IP not set - trying to get IP by myself"
   EXTERNAL_IP=$(curl -f icanhazip.com)
   export EXTERNAL_IP
 fi
 
-if [ -z "${DNSDIST_WEBSERVER_PASSWORD}" ];
-then
+if [ -z "${DNSDIST_WEBSERVER_PASSWORD}" ]; then
   echo "[INFO] Dnsdist webserver password not set - generating one"
   DNSDIST_WEBSERVER_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c12)
   export DNSDIST_WEBSERVER_PASSWORD
-  echo "[INFO] Generated WebServer Password: $DNSDIST_WEBSERVER_PASSWORD"
 fi
 
-if [ -z "${DNSDIST_WEBSERVER_API_KEY}" ];
-then
+if [ -z "${DNSDIST_WEBSERVER_API_KEY}" ]; then
   echo "[INFO] Dnsdist webserver api key not set - generating one"
   DNSDIST_WEBSERVER_API_KEY=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)
   export DNSDIST_WEBSERVER_API_KEY
-  echo "[INFO] Generated WebServer API Key: $DNSDIST_WEBSERVER_API_KEY"
 fi
 
-if [ "$INSTALL_DEFAULT_DOMAINS" = true ];
-then
+if [ "$INSTALL_DEFAULT_DOMAINS" = true ]; then
   echo "[INFO] Installing default domains..."
-  cp -v /var/lib/snidust/domains.d/*.lst /etc/snidust/domains.d/
+  cp -vn /var/lib/snidust/domains.d/*.lst /etc/snidust/domains.d/ 2>/dev/null || true
 fi
+
+# ایجاد فایل کلاینت‌ها در صورتی که وجود نداشته باشد
+if [ ! -f "/etc/snidust/clients.txt" ]; then
+  echo "$ALLOWED_CLIENTS" | tr ',' '\n' | sed 's/^[ \t]*//;s/[ \t]*$//' > /etc/snidust/clients.txt
+fi
+export ALLOWED_CLIENTS_FILE="/etc/snidust/clients.txt"
 
 echo "[INFO] Generating ACL..."
 set +e
 source generateACL.sh
 set -e
 
-
 echo "[INFO] Generating DNSDist Config..."
 /bin/bash /etc/dnsdist/dnsdist.conf.template > /etc/dnsdist/dnsdist.conf
 
-if [ "$DYNDNS_CRON_ENABLED" = true ];
-then
+if [ "$DYNDNS_CRON_ENABLED" = true ]; then
   echo "[INFO] DynDNS Address in ALLOWED_CLIENTS detected => Enable cron job"
   echo "$DYNDNS_CRON_SCHEDULE /bin/bash /dynDNSCron.sh" > /etc/snidust/dyndns.cron
   supercronic /etc/snidust/dyndns.cron &
@@ -66,6 +63,8 @@ fi
 echo "[INFO] Starting DNSDist..."
 /usr/bin/dnsdist -C /etc/dnsdist/dnsdist.conf --supervised --disable-syslog --uid snidust --gid snidust &
 
+echo "[INFO] Starting Web Panel..."
+python3 /webpanel.py &
 
 echo "[INFO] Starting nginx.."
 nginx
@@ -74,6 +73,7 @@ nginx_processId=$!
 sleep 5
 
 echo "==================================================================="
-echo "[INFO] SniDust started => Using $EXTERNAL_IP - Point your DNS settings to this address"
+echo "[INFO] SniDust started => Using $EXTERNAL_IP"
+echo "[INFO] WebPanel active on port 8085"
 echo "==================================================================="
 wait $nginx_processId
